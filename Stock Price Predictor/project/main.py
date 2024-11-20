8e9bee877810aad653fc1f2ff8a33f033066c0b3
RodnyllV <155683600+RodnyllV@users.noreply.github.com>
2024-11-19 19:41:22 -0500

    Update main.py

--- Stock Price Predictor/project/main.py
+++ Stock Price Predictor/project/main.py
@@ -2,40 +2,84 @@ import numpy
 import pandas
 import matplotlib.pyplot as pyPlot
 import seaborn
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
+
+import tensorflow
+import keras
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
-from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime
 
 import warnings
 warnings.filterwarnings("ignore")
 
-teslaDataFrame = pandas.read_csv("project/data/tesla.csv")
-teslaStockDateSplit = teslaDataFrame["Date"].str.split('/', expand = True)
+# Get data
+teslaDF = pandas.read_csv("project/data/tesla.csv")
+teslaDF = teslaDF.iloc[::-1]
+teslaDF["date"] = pandas.to_datetime(teslaDF["date"])
 
-teslaStockDateSplit["Day"] = teslaStockDateSplit[1].asType("int")
-teslaStockDateSplit["Month"] = teslaStockDateSplit[0].asType("int")
-teslaStockDateSplit["Year"] = teslaStockDateSplit[2].asType("int")
+predictionRange = teslaDF.loc[(teslaDF["date"] > datetime(2015, 1, 1)) & (teslaDF["date"] < datetime(2018, 1, 1))] 
+closeData = teslaDF.filter(["close"])
+dataSet = closeData.values
+trainingDataLength = int(numpy.ceil(len(dataSet) * 0.95))
 
-"""
-graphs = { # specifies the y axis
-    "open": "$USD",
-    "close": "$USD",
-    "high": "$USD",
-    "low": "$USD",
-    "volume": "SHARES"
-}
-"""
+# Scale the data
+scaler = MinMaxScaler(feature_range = (0, 1))
+scaledData = scaler.fit_transform(dataSet)
 
-graphs = ["open", "close", "high", "low", "volume"]
+# Prepare features and labels
+trainingData = scaledData[0:trainingDataLength, :]
+xTrain, yTrain = [], []
 
-_, ax = pyPlot.subplots(figsize = (20, 10))
+for i in range(60, len(trainingData)):
+    xTrain.append(trainingData[i-60:i, 0])
+    yTrain.append(trainingData[i, 0])
 
-for subCt, column in enumerate(graphs):
-    pyPlot.subplot(2, 3, subCt + 1)
-    seaborn.boxplot(teslaDataFrame[column], ax = ax)
+xTrain, yTrain = numpy.array(xTrain), numpy.array(yTrain)
+xTrain = numpy.reshape(xTrain, (xTrain.shape[0], xTrain.shape[1], 1))
+
+# Create and train the machine learning model
+trainingModel = keras.models.Sequential()
+trainingModel.add(keras.layers.LSTM(
+    units = 64,
+    return_sequences = True,
+    input_shape = (xTrain.shape[1], 1)
+))
+trainingModel.add(keras.layers.LSTM(units = 64))
+trainingModel.add(keras.layers.Dense(32))
+trainingModel.add(keras.layers.Dropout(0.5))
+trainingModel.add(keras.layers.Dense(1))
+
+trainingModel.compile(optimizer = 'adam', loss = 'mean_squared_error')
+history = trainingModel.fit(xTrain, yTrain, epochs = 10)
+
+# Create testing data
+testData = scaledData[trainingDataLength - 60:, :]
+xTest, yTest = [], dataSet[trainingDataLength:, :]
+
+for i in range(60, len(testData)):
+    xTest.append(testData[i - 60:i, 0])
+
+xTest = numpy.array(xTest)
+xTest = numpy.reshape(xTest, (xTest.shape[0], xTest.shape[1], 1))
+
+# Predict based off the testing data
+predictions = scaler.inverse_transform(trainingModel.predict(xTest))
+
+# Graph the predictions
+train = teslaDF[:trainingDataLength]
+test = teslaDF[trainingDataLength:]
+test["predictions"] = predictions
+
+pyPlot.figure(figsize = (10, 8))
+pyPlot.plot(train['date'], train['close'])
+pyPlot.plot(test['date'], test[['close', 'predictions']])
+pyPlot.title("Tesla Stock Closing Price")
+pyPlot.xlabel("Date")
+pyPlot.ylabel("Close")
+pyPlot.legend(["Train", "Test", "Predictions"])
 
 pyPlot.show()
